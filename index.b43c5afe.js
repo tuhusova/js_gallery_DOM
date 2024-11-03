58d0532e46e737ed080ae9501dcbf830b7b0c4dd
Anna Tuhusova <bilachayka@mail.com>
2024-11-03 20:25:21 +0100

    make build

---
+++ index.b43c5afe.js
@@ -0,0 +1,2 @@
+var e=document.getElementById("thumbs"),t=document.getElementById("largeImg");e.addEventListener("click",function(e){var l=e.target.closest(".list-item__link");l&&(e.preventDefault(),t.src=l.href,t.alt=l.title)});
+//# sourceMappingURL=index.b43c5afe.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,13 @@
+"use strict";
+const thumbs = document.getElementById("thumbs");
+const largeImg = document.getElementById("largeImg");
+thumbs.addEventListener("click", (ev)=>{
+    const imgLink = ev.target.closest(".list-item__link");
+    if (imgLink) {
+        ev.preventDefault();
+        largeImg.src = imgLink.href;
+        largeImg.alt = imgLink.title;
+    }
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
